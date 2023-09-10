20a451223546c6140b4ac2210ac9c3298c10fdb9
wildanGhifari <wildghifari@gmail.com>
2023-09-11 01:09:31 +0700

    feat: implement select image method from camera and gallery

--- lib/screens/add_post.dart
+++ lib/screens/add_post.dart
@@ -1,4 +1,8 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:instagram_clone/utils/utils.dart';
 
 class AddPost extends StatefulWidget {
   const AddPost({super.key});
@@ -8,67 +12,112 @@ class AddPost extends StatefulWidget {
 }
 
 class _AddPostState extends State<AddPost> {
+  final TextEditingController _captionController = TextEditingController();
+  Uint8List? _file;
+
+  _selectImage(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return SimpleDialog(
+          title: const Text("Add New Post"),
+          children: [
+            SimpleDialogOption(
+              padding: const EdgeInsets.all(20),
+              child: const Text("Take a photo"),
+              onPressed: () async {
+                Navigator.of(context).pop();
+                Uint8List file = await pickImage(ImageSource.camera);
+                setState(() {
+                  _file = file;
+                });
+              },
+            ),
+            SimpleDialogOption(
+              padding: const EdgeInsets.all(20),
+              child: const Text("Choose from gallery"),
+              onPressed: () async {
+                Navigator.of(context).pop();
+                Uint8List file = await pickImage(ImageSource.gallery);
+                setState(() {
+                  _file = file;
+                });
+              },
+            ),
+            SimpleDialogOption(
+              padding: const EdgeInsets.all(20),
+              child: const Text("Cancel"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return Column(
-    //   mainAxisAlignment: MainAxisAlignment.center,
-    //   children: [
-    //     ElevatedButton(
-    //       onPressed: () {},
-    //       child: const Text("Add New Post"),
-    //     )
-    //   ],
-    // );
-
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios_new_rounded),
-          onPressed: () {},
-        ),
-        title: const Text("New Post"),
-        centerTitle: false,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextButton(
-              onPressed: () {},
-              child: const Text("Post"),
-            ),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          SizedBox(
-            width: double.infinity,
-            child: AspectRatio(
-              aspectRatio: 3 / 2,
-              child: Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(
-                        "https://images.unsplash.com/photo-1694166572428-683d4bc6e989?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2487&q=80"),
-                    fit: BoxFit.cover,
-                    alignment: FractionalOffset.center,
+    return _file == null
+        ? Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => _selectImage(context),
+                child: const Text("Add New Post"),
+              )
+            ],
+          )
+        : Scaffold(
+            appBar: AppBar(
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back_ios_new_rounded),
+                onPressed: () {},
+              ),
+              title: const Text("New Post"),
+              centerTitle: false,
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text("Post"),
                   ),
                 ),
-              ),
+              ],
             ),
-          ),
-          const SizedBox(height: 16),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: const TextField(
-              maxLines: 8,
-              decoration: InputDecoration(
-                hintText: "Write a caption...",
-                border: InputBorder.none,
-              ),
+            body: Column(
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: AspectRatio(
+                    aspectRatio: 3 / 2,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: MemoryImage(_file!),
+                          fit: BoxFit.cover,
+                          alignment: FractionalOffset.center,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: TextField(
+                    controller: _captionController,
+                    maxLines: 8,
+                    decoration: const InputDecoration(
+                      hintText: "Write a caption...",
+                      border: InputBorder.none,
+                    ),
+                  ),
+                )
+              ],
             ),
-          )
-        ],
-      ),
-    );
+          );
   }
 }
